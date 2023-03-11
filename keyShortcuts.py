82db63381a468c13406ce5e35db8e1bc903c4f2a
mirtchev <mirtcht@algonquincollege.com>
2023-03-11 20:59:01 +0000

    First

--- keyShortcuts.py
+++ keyShortcuts.py
@@ -7,10 +7,11 @@ def main(page: ft.Page):
                 f"Key: {e.key}, Shift: {e.shift}, Control: {e.ctrl}, Alt: {e.alt}, Meta: {e.meta}"
             )
         )
+        
 
     page.on_keyboard_event = on_keyboard
     page.add(
         ft.Text("Press any key with a combination of CTRL, ALT, SHIFT and META keys...")
     )
 
-ft.app(target=main)
+ft.app(target=main, view=ft.WEB_BROWSER)
