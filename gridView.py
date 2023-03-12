8b46713cf3890fb64ae8005bf534fa141824b796
mirtchev <mirtcht@algonquincollege.com>
2023-03-12 20:10:09 +0000

    dynamic

--- gridView.py
+++ gridView.py
@@ -6,19 +6,29 @@ import time
 os.environ["FLET_WS_MAX_MESSAGE_SIZE"] = "8000000"
 
 def main(page: ft.Page):
+    
+    def change(e: ft.ControlEvent ):
+        e.control.bgcolor=ft.colors.WHITE
+        e.control.shape=ft.BoxShape.CIRCLE
+        page.update()
+
     gv = ft.GridView(expand=True, max_extent=150, child_aspect_ratio=1)
     page.add(gv)
 
     clrs = [ft.colors.RED, ft.colors.GREEN, ft.colors.BLUE]
 
     for i in range(30):
+        ftt=ft.Text(f"Item {i}")
+
         gv.controls.append(
             ft.Container(
-                ft.Text(f"Item {i}"),
+                ftt,
                 alignment=ft.alignment.center,
                 bgcolor= clrs[i%3],
                 border=ft.border.all(1, ft.colors.AMBER_400),
                 border_radius=ft.border_radius.all(5),
+                on_click=change,
+                ink=True
             )
         )
 
