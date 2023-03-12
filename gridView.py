b20faacd0a5446c1d54d97c1896b514c2951ce04
mirtchev <mirtcht@algonquincollege.com>
2023-03-12 18:07:58 +0000

    save

--- gridView.py
+++ gridView.py
@@ -1,5 +1,7 @@
 import os
 import flet as ft
+import random
+import time
 
 os.environ["FLET_WS_MAX_MESSAGE_SIZE"] = "8000000"
 
@@ -7,16 +9,20 @@ def main(page: ft.Page):
     gv = ft.GridView(expand=True, max_extent=150, child_aspect_ratio=1)
     page.add(gv)
 
-    for i in range(5000):
+    clrs = [ft.colors.RED, ft.colors.GREEN, ft.colors.BLUE]
+
+    for i in range(3):
         gv.controls.append(
             ft.Container(
                 ft.Text(f"Item {i}"),
                 alignment=ft.alignment.center,
-                bgcolor=ft.colors.AMBER_100,
+                bgcolor= clrs[i%3],
                 border=ft.border.all(1, ft.colors.AMBER_400),
                 border_radius=ft.border_radius.all(5),
             )
         )
+
     page.update()
 
 ft.app(target=main, view=ft.WEB_BROWSER)
+#ft.app(target=main)
